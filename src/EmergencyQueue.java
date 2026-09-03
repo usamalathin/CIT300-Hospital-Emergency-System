96f87131cbdafc9c858f1b59db5c5af3e736ebb9
MUM. Usama Lathin <usamalathin9@gmail.com>
2026-09-03 20:06:39 +0530

    Added queue enqueue operation

--- src/EmergencyQueue.java
+++ src/EmergencyQueue.java
@@ -8,4 +8,13 @@ public class EmergencyQueue {
     public EmergencyQueue() {
         emergencyQueue = new LinkedList<>();
     }
+
+    public void enqueuePatient(Patient patient) {
+
+        emergencyQueue.add(patient);
+
+        System.out.println(
+            patient.patientName + " added to emergency queue."
+        );
+    }
 }
