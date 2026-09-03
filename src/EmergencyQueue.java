7a277a2f610c7b51c1e7f4b634bbf0cad097689d
MUM. Usama Lathin <usamalathin9@gmail.com>
2026-09-03 20:00:14 +0530

    Created emergency queue structure

--- src/EmergencyQueue.java
+++ src/EmergencyQueue.java
@@ -1,3 +1,11 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class EmergencyQueue {
-    
+
+    Queue<Patient> emergencyQueue;
+
+    public EmergencyQueue() {
+        emergencyQueue = new LinkedList<>();
+    }
 }
