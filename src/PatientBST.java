82424e78cab28f3b56c6e18797490b1aad231007
MUM. Usama Lathin <usamalathin9@gmail.com>
2026-09-03 14:14:14 +0530

    Created BST node structure

--- src/PatientBST.java
+++ src/PatientBST.java
@@ -1,3 +1,21 @@
 public class PatientBST {
-    
+
+    class Node {
+
+        Patient patient;
+        Node left;
+        Node right;
+
+        public Node(Patient patient) {
+            this.patient = patient;
+            left = null;
+            right = null;
+        }
+    }
+
+    Node root;
+
+    public PatientBST() {
+        root = null;
+    }
 }
