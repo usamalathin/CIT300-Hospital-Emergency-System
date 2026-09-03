1ceea8eb87ab0a25f5d895f76fd2b9c5804ab38f
MUM. Usama Lathin <usamalathin9@gmail.com>
2026-09-03 23:00:27 +0530

    Created linked list structure

--- src/VisitLinkedList.java
+++ src/VisitLinkedList.java
@@ -1,3 +1,30 @@
 public class VisitLinkedList {
-    
+
+    class VisitNode {
+
+        int visitId;
+        String visitDate;
+        String doctorName;
+        String diagnosis;
+        String treatment;
+
+        VisitNode next;
+
+        public VisitNode(
+                int visitId,
+                String visitDate,
+                String doctorName,
+                String diagnosis,
+                String treatment) {
+
+            this.visitId = visitId;
+            this.visitDate = visitDate;
+            this.doctorName = doctorName;
+            this.diagnosis = diagnosis;
+            this.treatment = treatment;
+            this.next = null;
+        }
+    }
+
+    VisitNode head = null;
 }
