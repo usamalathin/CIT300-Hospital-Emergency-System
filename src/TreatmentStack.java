147b60029717374a3789298577e542e0659f98ba
MUM. Usama Lathin <usamalathin9@gmail.com>
2026-09-03 20:23:29 +0530

    Created treatment stack structure

--- src/TreatmentStack.java
+++ src/TreatmentStack.java
@@ -1,3 +1,11 @@
+import java.util.Stack;
+
 public class TreatmentStack {
-    
+
+    Stack<Patient> treatmentStack;
+
+    public TreatmentStack() {
+
+        treatmentStack = new Stack<>();
+    }
 }
